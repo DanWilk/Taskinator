40b9250a062c3a1b3e1a8e91d3a38d1f16e9ac99
DanWilk <dcwilkinson@wilktech.com>
2021-09-26 13:48:03 -0400

    add unique task ids

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var tasksToDoEL = document.querySelector('#tasks-to-do');
 
@@ -30,12 +31,37 @@ var createTaskEl = function(taskDataObj) {
     var listItemEl = document.createElement('li');
     listItemEl.className = 'task-item';
 
+    listItemEl.setAttribute('data-task-id', taskIdCounter);
+
     var taskInfoEl = document.createElement('div');
     taskInfoEl.className = 'task-info';
     taskInfoEl.innerHTML = '<h3 class="task-name">' + taskDataObj.name + '</h3><span class="task-type">' + taskDataObj.type + '</span>';
     listItemEl.appendChild(taskInfoEl);
 
     tasksToDoEL.appendChild(listItemEl);
+
+    taskIdCounter++;
 }
+
+var createTaskActions = function(taskId) {
+    var actionContainerEl = document.createElement('div');
+    actionContainerEl.className = 'task-actions';
+
+    var editButtonEl = document.createElement('button');
+    editButtonEl.textContent = 'Edit';
+    editButtonEl.className = 'btn edit-btn';
+    editButtonEl.setAttribute('data-task-id', taskId);
+
+    actionContainerEl.appendChild(editButtonEl);
+
+    var deleteButtonEl = document.createElement('button');
+    deleteButtonEl.textContent = 'Delete';
+    deleteButtonEl.className = 'btn delete-btn';
+    deleteButtonEl.setAttribute('data-task-id', taskId);
+
+    actionContainerEl.appendChild(deleteButtonEl);
+
+    return actionContainerEl;
+};
 
 formEl.addEventListener('submit', taskFormHandler);
